7e80c7be051a25a318cc52829806888a8212b06f
Daniel Lopes <danielpvpao@gmail.com>
2025-02-12 12:36:51 -0300

    Change database URL for production deployment

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,7 @@
 require('dotenv').config()
 module.exports = {
   dialect: 'postgres',
-  port: process.env.DATABASE_PORT,
-  username: process.env.DATABASE_USERNAME,
-  host: process.env.DATABASE_HOST,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
+  url: process.env.DATABASE_URL,
   define: {
     timestamps: true,
     underscored: true,
