f474adf5f95e9b11d53d0b392031dda3c8432893
Daniel Lopes <danielpvpao@gmail.com>
2025-02-12 22:45:42 -0300

    fix: recreate __dirname for ESM compatibility

--- src/controllers/MusicController.js
+++ src/controllers/MusicController.js
@@ -4,6 +4,10 @@ import ytdl from '@distube/ytdl-core'
 import fs from 'fs'
 import cloudinary from '../config/cloudinary.js'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export class MusicController {
   async download(req, res) {
