2a6c91da7b7e5d2e9b15a83ce7b45b96e28815f2
Daniel Lopes <danielpvpao@gmail.com>
2025-04-01 20:50:24 -0300

    test

--- src/routes/music.js
+++ src/routes/music.js
@@ -1,19 +1,10 @@
 import { Router } from 'express'
-import multer from 'multer'
 import musicController from '../controllers/MusicController.js'
-import multerConfig from '../config/multer.js'
 import loginRequired from '../middlewares/loginRequired.js'
-
-const upload = multer(multerConfig)
 
 const router = Router()
 
-router.post(
-  '/',
-  loginRequired,
-  upload.single('Music'),
-  musicController.download,
-)
+router.post('/', loginRequired, musicController.download)
 router.get('/', musicController.index)
 router.delete('/', loginRequired, musicController.delete)
 router.put('/', loginRequired, musicController.syncWithUser)
