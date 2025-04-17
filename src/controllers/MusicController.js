132e974e7c7e42b1b35d422e8049d51b73dfb6d0
Daniel Lopes <danielpvpao@gmail.com>
2025-04-17 14:18:28 -0300

    a

--- src/controllers/MusicController.js
+++ src/controllers/MusicController.js
@@ -26,7 +26,7 @@ export class MusicController {
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
         },
       })
-
+      //
       const user = await User.findByPk(req.userId)
       const music = Music
       if (!user) {
