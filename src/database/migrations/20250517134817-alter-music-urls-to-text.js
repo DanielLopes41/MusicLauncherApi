c34720eb1b7369212b48ed740fb1b90006290db6
Daniel Lopes <danielpvpao@gmail.com>
2025-05-17 11:00:03 -0300

    t6

---
+++ src/database/migrations/20250517134817-alter-music-urls-to-text.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('musics', 'cloudinaryUrl', {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    })
+
+    await queryInterface.changeColumn('musics', 'thumbnailUrl', {
+      type: Sequelize.TEXT,
+      allowNull: true,
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('musics', 'cloudinaryUrl', {
+      type: Sequelize.STRING,
+      allowNull: false,
+    })
+
+    await queryInterface.changeColumn('musics', 'thumbnailUrl', {
+      type: Sequelize.STRING,
+      allowNull: true,
+    })
+  },
+}
